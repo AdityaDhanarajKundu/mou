2f7ced3df4e0cdd616c2bb9a3ba6302f2c39eefc
AdityaDhanaraj <adityadhanarajkundu@gmail.com>
2023-10-07 17:54:40 +0530

    personal info

--- src/mou-form.jsx
+++ src/mou-form.jsx
@@ -1,7 +1,32 @@
-import React from 'react';
+import {useState} from 'react';
 
 export default function MouForm(){
+    const [partner1, setPartner1] = useState('');
+    const [partner2, setPartner2] = useState('');
+
+    function handleChange1(e){
+        setPartner1(e.target.value);
+    }
+    function handleChange2(e){
+        setPartner2(e.target.value);
+    }
+    
     return(
-        <h2>Hello, World</h2>
+        <div className="mou-form">
+            <h2>Memorandum of Understanding</h2>
+
+            <div className="partner-info">
+                <p>Between</p>
+                <input type="text" placeholder="(Partner)" value={partner1} onChange={handleChange1}/>
+                <p>And</p>
+                <input type="text" placeholder="(Partner)" value={partner2} onChange={handleChange2} />
+                {partner1 && partner2 ? <h4>This Memorandum of Understanding(MOU) sets for the terms and understanding between {partner1} and {partner2}</h4> : null}
+                
+            </div>
+            <h3>Background</h3>
+
+            <textarea placeholder="Enter background information here..."></textarea>
+
+        </div>
     );
 }
