9844165130313462dec5e32ff14e9d29ce81bb00
AdityaDhanaraj <adityadhanarajkundu@gmail.com>
2023-10-07 19:06:11 +0530

    added the basic components css needs to be done

--- src/mou-form.jsx
+++ src/mou-form.jsx
@@ -16,111 +16,119 @@ export default function MouForm() {
     setPurpose(e.target.value);
   }
 
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log("submitted");
+  }
+
   return (
-    <form className="mou-form">
+    <form className="mou-form" onSubmit={handleSubmit}>
       <h2>Memorandum of Understanding</h2>
 
-      <div className="partner-info">
-        <p>Between</p>
-        <input
-          type="text"
-          placeholder="(Partner)"
-          value={partner1}
-          onChange={handleChange1}
-        />
-        <p>And</p>
-        <input
-          type="text"
-          placeholder="(Partner)"
-          value={partner2}
-          onChange={handleChange2}
-        />
-        {partner1 && partner2 ? (
-          <h4>
-            This Memorandum of Understanding(MOU) sets for the terms and
-            understanding between {partner1} and {partner2}
-          </h4>
-        ) : null}
-      </div>
-      <div className="details">
-        <h3>Background</h3>
-        <textarea
-          placeholder="(Enter background information here...)"
-          rows={6}
-        ></textarea>
-        <h3>Purpose</h3>
-        <textarea
-          placeholder="(Purpose/goals of this partnership...)"
-          rows={6}
-          value={purpose}
-          onChange={handlePurpose}
-        ></textarea>
-        {purpose ? (
-          <>
+      <div className="main">
+        <div className="partner-info">
+          <p>Between</p>
+          <input
+            type="text"
+            placeholder="(Partner)"
+            value={partner1}
+            onChange={handleChange1}
+          />
+          <p>And</p>
+          <input
+            type="text"
+            placeholder="(Partner)"
+            value={partner2}
+            onChange={handleChange2}
+          />
+          {partner1 && partner2 ? (
             <h4>
-              The above goals will be achieved by undertaking the following
-              activities.
+              This Memorandum of Understanding(MOU) sets for the terms and
+              understanding between {partner1} and {partner2}
             </h4>
-            <CheckBox /> {/*To be changed*/}
-          </>
-        ) : null}
-        <h3>Reporting</h3>
-        <textarea
-          placeholder="(Record who will evaluate effectiveness and adherence to the agreement and when evaluation will happen...)"
-          rows={6}
-        ></textarea>
-        <h3>Funding</h3>
-        <textarea
-          placeholder="(Specify that this MOU is not a commitment of funds...)"
-          rows={6}
-        ></textarea>
-        <h3>Duration</h3>
-        <p>
-          This MOU is at-will and may be modified by mutual consent of
-          authorized officials from (list partners).{" "}
-          {/* List partners to be changed */}
-          This MOU shall become effective upon signature by the authorized
-          officials from the (list partners) and will remain in effect until
-          modified or terminated by any one of the partners by mutual consent.
-          In the absence of mutual agreement by the authorized officials from
-          (list partners) this MOU shall end on <input type="date" />.
-        </p>
-      </div>
-      <div className="contact-info">
-        <div className="partner">
-          <p>Partner Name</p>
-          <input type="text" placeholder="(Partner Name)" />
-          <p>Partner Representative</p>
-          <input type="text" placeholder="(Partner Representative)" />
-          <p>Position</p>
-          <input type="text" placeholder="(Position)" />
-          <p>Address</p>
-          <input type="text" placeholder="(Address)" />
-          <p>Phone</p>
-          <input type="text" placeholder="(Phone)" />
-          <p>Fax</p>
-          <input type="text" placeholder="(Fax)" />
-          <p>Email</p>
-          <input type="text" placeholder="(Email)" />
+          ) : null}
         </div>
+        <div className="details">
+          <h3>Background</h3>
+          <textarea
+            placeholder="(Enter background information here...)"
+            rows={6}
+          ></textarea>
+          <h3>Purpose</h3>
+          <textarea
+            placeholder="(Purpose/goals of this partnership...)"
+            rows={6}
+            value={purpose}
+            onChange={handlePurpose}
+          ></textarea>
+          {purpose ? (
+            <>
+              <h4>
+                The above goals will be achieved by undertaking the following
+                activities.
+              </h4>
+              <CheckBox /> {/*To be changed*/}
+            </>
+          ) : null}
+          <h3>Reporting</h3>
+          <textarea
+            placeholder="(Record who will evaluate effectiveness and adherence to the agreement and when evaluation will happen...)"
+            rows={6}
+          ></textarea>
+          <h3>Funding</h3>
+          <textarea
+            placeholder="(Specify that this MOU is not a commitment of funds...)"
+            rows={6}
+          ></textarea>
+          <h3>Duration</h3>
+          <p>
+            This MOU is at-will and may be modified by mutual consent of
+            authorized officials from (list partners).{" "}
+            {/* List partners to be changed */}
+            This MOU shall become effective upon signature by the authorized
+            officials from the (list partners) and will remain in effect until
+            modified or terminated by any one of the partners by mutual consent.
+            In the absence of mutual agreement by the authorized officials from
+            (list partners) this MOU shall end on <input type="date" />.
+          </p>
+        </div>
+        <div className="contact-info">
+          <div className="partner">
+            <p>Partner Name</p>
+            <input type="text" placeholder="(Partner Name)" />
+            <p>Partner Representative</p>
+            <input type="text" placeholder="(Partner Representative)" />
+            <p>Position</p>
+            <input type="text" placeholder="(Position)" />
+            <p>Address</p>
+            <input type="text" placeholder="(Address)" />
+            <p>Phone</p>
+            <input type="text" placeholder="(Phone)" />
+            <p>Fax</p>
+            <input type="text" placeholder="(Fax)" />
+            <p>Email</p>
+            <input type="text" placeholder="(Email)" />
+          </div>
 
-        <div className="partner">
-          <p>Partner Name</p>
-          <input type="text" placeholder="(Partner Name)" />
-          <p>Partner Representative</p>
-          <input type="text" placeholder="(Partner Representative)" />
-          <p>Position</p>
-          <input type="text" placeholder="(Position)" />
-          <p>Address</p>
-          <input type="text" placeholder="(Address)" />
-          <p>Phone</p>
-          <input type="text" placeholder="(Phone)" />
-          <p>Fax</p>
-          <input type="text" placeholder="(Fax)" />
-          <p>Email</p>
-          <input type="text" placeholder="(Email)" />
+          <div className="partner">
+            <p>Partner Name</p>
+            <input type="text" placeholder="(Partner Name)" />
+            <p>Partner Representative</p>
+            <input type="text" placeholder="(Partner Representative)" />
+            <p>Position</p>
+            <input type="text" placeholder="(Position)" />
+            <p>Address</p>
+            <input type="text" placeholder="(Address)" />
+            <p>Phone</p>
+            <input type="text" placeholder="(Phone)" />
+            <p>Fax</p>
+            <input type="text" placeholder="(Fax)" />
+            <p>Email</p>
+            <input type="text" placeholder="(Email)" />
+          </div>
         </div>
       </div>
+      <input type="submit" value="Submit" />
     </form>
   );
 }
