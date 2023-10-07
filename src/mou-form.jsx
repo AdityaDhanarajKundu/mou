fec9898f100b6ed4f11bb44846ed2f70d35b959a
AdityaDhanaraj <adityadhanarajkundu@gmail.com>
2023-10-07 18:45:19 +0530

    added the segment of partner contact

--- src/mou-form.jsx
+++ src/mou-form.jsx
@@ -17,7 +17,7 @@ export default function MouForm() {
   }
 
   return (
-    <div className="mou-form">
+    <form className="mou-form">
       <h2>Memorandum of Understanding</h2>
 
       <div className="partner-info">
@@ -61,10 +61,66 @@ export default function MouForm() {
               The above goals will be achieved by undertaking the following
               activities.
             </h4>
-            <CheckBox />
+            <CheckBox /> {/*To be changed*/}
           </>
         ) : null}
+        <h3>Reporting</h3>
+        <textarea
+          placeholder="(Record who will evaluate effectiveness and adherence to the agreement and when evaluation will happen...)"
+          rows={6}
+        ></textarea>
+        <h3>Funding</h3>
+        <textarea
+          placeholder="(Specify that this MOU is not a commitment of funds...)"
+          rows={6}
+        ></textarea>
+        <h3>Duration</h3>
+        <p>
+          This MOU is at-will and may be modified by mutual consent of
+          authorized officials from (list partners).{" "}
+          {/* List partners to be changed */}
+          This MOU shall become effective upon signature by the authorized
+          officials from the (list partners) and will remain in effect until
+          modified or terminated by any one of the partners by mutual consent.
+          In the absence of mutual agreement by the authorized officials from
+          (list partners) this MOU shall end on <input type="date" />.
+        </p>
       </div>
-    </div>
+      <div className="contact-info">
+        <div className="partner">
+          <p>Partner Name</p>
+          <input type="text" placeholder="(Partner Name)" />
+          <p>Partner Representative</p>
+          <input type="text" placeholder="(Partner Representative)" />
+          <p>Position</p>
+          <input type="text" placeholder="(Position)" />
+          <p>Address</p>
+          <input type="text" placeholder="(Address)" />
+          <p>Phone</p>
+          <input type="text" placeholder="(Phone)" />
+          <p>Fax</p>
+          <input type="text" placeholder="(Fax)" />
+          <p>Email</p>
+          <input type="text" placeholder="(Email)" />
+        </div>
+
+        <div className="partner">
+          <p>Partner Name</p>
+          <input type="text" placeholder="(Partner Name)" />
+          <p>Partner Representative</p>
+          <input type="text" placeholder="(Partner Representative)" />
+          <p>Position</p>
+          <input type="text" placeholder="(Position)" />
+          <p>Address</p>
+          <input type="text" placeholder="(Address)" />
+          <p>Phone</p>
+          <input type="text" placeholder="(Phone)" />
+          <p>Fax</p>
+          <input type="text" placeholder="(Fax)" />
+          <p>Email</p>
+          <input type="text" placeholder="(Email)" />
+        </div>
+      </div>
+    </form>
   );
 }
